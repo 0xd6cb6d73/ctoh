ef9fbd90d0391dc64a7ce820b86d848e50ca2d47
0xd6cb6d73 <72226198+0xd6cb6d73@users.noreply.github.com>
2026-02-16 22:35:16 +0100

    Initial version. Only parse struct and function declarations

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,108 @@
 #include <clang-c/Index.h>
 #include <cstdint>
 #include <format>
+#include <fstream>
 #include <iostream>
 #include <stdexcept>
 #include <string>
+#include <vector>
+
+const std::string LIT_STRUCT("struct ");
+const std::string LIT_BRACE_OPEN("{");
+const std::string LIT_BRACE_CLOSE("}");
+const std::string LIT_PAREN_OPEN("(");
+const std::string LIT_PAREN_CLOSE(")");
+const std::string LIT_SEMI_COLON(";");
+const std::string LIT_COMMA(",");
+const std::string LIT_LF("\n");
+
+struct ParseData {
+  CXTranslationUnit in_tu;
+  std::fstream *fstr;
+};
+
+struct HeaderType {
+  std::string name;
+  CXType type;
+};
+
+struct HeaderStruct {
+  std::string name;
+  std::vector<HeaderType> types;
+};
+
+struct FunctionArg {
+  std::string name;
+  CXType type;
+};
+
+struct Function {
+  std::string name;
+  CXType ret;
+  std::vector<FunctionArg> args;
+};
+
+namespace ctoh {
+HeaderStruct dump_struct(CXCursor cursor) {
+  HeaderStruct data{};
+  CXString struct_name = clang_getCursorSpelling(cursor);
+  data.name = std::string(clang_getCString(struct_name));
+  clang_disposeString(struct_name);
+
+  clang_visitChildren(
+      cursor,
+      [](CXCursor current_cursor, CXCursor parent, CXClientData client_data) {
+        HeaderStruct *out = reinterpret_cast<HeaderStruct *>(client_data);
+        CXCursorKind kind = clang_getCursorKind(current_cursor);
+        if (kind == CXCursor_FieldDecl) {
+          CXString field_name = clang_getCursorSpelling(current_cursor);
+          CXType field_type = clang_getCursorType(current_cursor);
+          CXString type_name = clang_getTypeSpelling(field_type);
+          out->types.push_back(
+              {.name = std::string(std::string(clang_getCString(field_name))), .type = field_type});
+          clang_disposeString(type_name);
+          clang_disposeString(field_name);
+        }
+        return CXChildVisit_Recurse;
+      },
+      &data);
+  return data;
+}
+
+Function get_fn_sig(CXCursor cursor) {
+  CXString fn_name = clang_getCursorSpelling(cursor);
+
+  CXCursor args_cursor = clang_Cursor_getArgument(cursor, 0);
+  Function data{};
+  data.ret = clang_getCursorResultType(cursor);
+  data.name = std::string(clang_getCString(fn_name));
+  clang_disposeString(fn_name);
+  clang_visitChildren(
+      cursor,
+      [](CXCursor current_cursor, CXCursor parent, CXClientData client_data) {
+        Function *out = reinterpret_cast<Function *>(client_data);
+        CXCursorKind kind = clang_getCursorKind(current_cursor);
+        if (kind == CXCursor_ParmDecl) {
+          CXString param_name = clang_getCursorSpelling(current_cursor);
+          CXType param_type = clang_getCursorType(current_cursor);
+          CXString type_name = clang_getTypeSpelling(param_type);
+          out->args.push_back(
+              {.name = std::string(std::string(clang_getCString(param_name))), .type = param_type});
+          clang_disposeString(type_name);
+          clang_disposeString(param_name);
+        }
+        return CXChildVisit_Recurse;
+      },
+      &data);
+  return data;
+  std::cout << "Function: " << clang_getCString(fn_name);
+  clang_disposeString(fn_name);
+  CXType cursor_type = clang_getCursorType(cursor);
+  CXString type_spelling = clang_getTypeSpelling(cursor_type);
+  std::cout << ", namely " << clang_getCString(type_spelling);
+  clang_disposeString(type_spelling);
+}
+} // namespace ctoh
 
 int32_t main(int32_t argc, char *argv[]) {
   if (argc < 2) {
@@ -17,86 +116,74 @@ int32_t main(int32_t argc, char *argv[]) {
 
   CXIndex index = clang_createIndex(0, 1);
   CXTranslationUnit tu{};
-  CXErrorCode err = clang_parseTranslationUnit2(index, file_name.data(),
-                                                nullptr, 0, nullptr, 0, 0, &tu);
+  CXErrorCode err =
+      clang_parseTranslationUnit2(index, file_name.data(), nullptr, 0, nullptr, 0, 0, &tu);
   if (err != 0) {
     throw std::runtime_error(std::format("TU parse failure: {}", int32_t(err)));
   }
 
   CXFile my_file = clang_getFile(tu, file_name.data());
   CXCursor cursor = clang_getTranslationUnitCursor(tu);
+
+  std::string header_name = std::format("{}.h", file_name);
+  std::fstream out(header_name, out.out | out.trunc);
+  ParseData parse_data{.in_tu = tu, .fstr = &out};
+
   clang_visitChildren(
       cursor,
       [](CXCursor current_cursor, CXCursor parent, CXClientData client_data) {
         CXType cursor_type = clang_getCursorType(current_cursor);
-        CXFile my_file = reinterpret_cast<CXFile *>(client_data);
-        CXFile file;
+        CXCursorKind cursor_kind = clang_getCursorKind(current_cursor);
+        ParseData *parse_data = reinterpret_cast<ParseData *>(client_data);
 
         CXSourceLocation loc = clang_getCursorLocation(current_cursor);
-        clang_getExpansionLocation(loc, &file, nullptr, nullptr, nullptr);
-        if (file != my_file) {
+        if (clang_Location_isFromMainFile(loc) == 0) {
           return CXChildVisit_Recurse;
         }
 
-        CXString type_kind_spelling =
-            clang_getTypeKindSpelling(cursor_type.kind);
-        std::cout << "TypeKind: " << clang_getCString(type_kind_spelling);
-        clang_disposeString(type_kind_spelling);
-
-        if (cursor_type.kind == CXType_Pointer || // If cursor_type is a pointer
-            cursor_type.kind ==
-                CXType_LValueReference || // or an LValue Reference (&)
-            cursor_type.kind ==
-                CXType_RValueReference) { // or an RValue Reference (&&),
-          CXType pointed_to_type =
-              clang_getPointeeType(cursor_type); // retrieve the pointed-to type
-
-          CXString pointed_to_type_spelling =
-              clang_getTypeSpelling(pointed_to_type); // Spell out the entire
-          std::cout << "pointing to type: "
-                    << clang_getCString(
-                           pointed_to_type_spelling); // pointed-to type
-          clang_disposeString(pointed_to_type_spelling);
-        } else if (cursor_type.kind == CXType_Record) {
-          CXString type_spelling = clang_getTypeSpelling(cursor_type);
-          std::cout << ", namely " << clang_getCString(type_spelling);
-          clang_disposeString(type_spelling);
+        if (cursor_kind == CXCursor_FunctionDecl) {
+          auto data = ctoh::get_fn_sig(current_cursor);
+          CXString ret_spelling = clang_getTypeSpelling(data.ret);
+          std::string ret_str(clang_getCString(ret_spelling));
+          parse_data->fstr->write(ret_str.data(), ret_str.size());
+          clang_disposeString(ret_spelling);
+          parse_data->fstr->write(" ", 1);
+          parse_data->fstr->write(data.name.data(), data.name.size());
+          parse_data->fstr->write(LIT_PAREN_OPEN.data(), LIT_PAREN_OPEN.size());
+          for (const auto &type : data.args) {
+            CXString spelling = clang_getTypeSpelling(type.type);
+            std::string spelling_str(clang_getCString(spelling));
+            parse_data->fstr->write(spelling_str.data(), spelling_str.size());
+            parse_data->fstr->write(" ", 1);
+            parse_data->fstr->write(type.name.data(), type.name.size());
+            parse_data->fstr->write(LIT_COMMA.data(), LIT_COMMA.size());
+            clang_disposeString(spelling);
+          }
+          parse_data->fstr->seekp(-1, std::ios_base::seekdir::end);
+          parse_data->fstr->write(LIT_PAREN_CLOSE.data(), LIT_PAREN_CLOSE.size());
+          parse_data->fstr->write(LIT_SEMI_COLON.data(), LIT_SEMI_COLON.size());
+        } else if (cursor_kind == CXCursor_StructDecl) {
+          HeaderStruct data = ctoh::dump_struct(current_cursor);
+          parse_data->fstr->write(LIT_STRUCT.data(), LIT_STRUCT.size());
+          parse_data->fstr->write(data.name.data(), data.name.size());
+          parse_data->fstr->write(LIT_BRACE_OPEN.data(), LIT_BRACE_OPEN.size());
+          parse_data->fstr->write(LIT_LF.data(), LIT_LF.size());
+          for (const auto &type : data.types) {
+            CXString spelling = clang_getTypeSpelling(type.type);
+            std::string spelling_str(clang_getCString(spelling));
+            parse_data->fstr->write(spelling_str.data(), spelling_str.size());
+            parse_data->fstr->write(" ", 1);
+            parse_data->fstr->write(type.name.data(), type.name.size());
+            parse_data->fstr->write(LIT_SEMI_COLON.data(), LIT_SEMI_COLON.size());
+            parse_data->fstr->write(LIT_LF.data(), LIT_LF.size());
+            clang_disposeString(spelling);
+          }
+          parse_data->fstr->write(LIT_BRACE_CLOSE.data(), LIT_BRACE_CLOSE.size());
+          parse_data->fstr->write(LIT_SEMI_COLON.data(), LIT_SEMI_COLON.size());
+          parse_data->fstr->write(LIT_LF.data(), LIT_LF.size());
+        } else if (cursor_kind == CXCursor_InclusionDirective) {
         }
-        std::cout << "\n";
         return CXChildVisit_Recurse;
       },
-      my_file);
-
-  clang_visitChildren(
-      cursor,
-      [](CXCursor current_cursor, CXCursor parent, CXClientData client_data) {
-        CXType cursor_type = clang_getCursorType(current_cursor);
-        CXString cursor_spelling = clang_getCursorSpelling(current_cursor);
-        CXSourceRange cursor_range = clang_getCursorExtent(current_cursor);
-        CXFile file;
-        CXFile my_file = reinterpret_cast<CXFile *>(client_data);
-        unsigned start_line, start_column, start_offset;
-        unsigned end_line, end_column, end_offset;
-
-        clang_getExpansionLocation(clang_getRangeStart(cursor_range), &file,
-                                   &start_line, &start_column, &start_offset);
-        CXString cx_file_name = clang_getFileName(file);
-        auto f_name = std::string(clang_getCString(cx_file_name));
-        if (file != my_file) {
-          return CXChildVisit_Recurse;
-        }
-
-        std::cout << "Cursor " << clang_getCString(cursor_spelling);
-        clang_getExpansionLocation(clang_getRangeEnd(cursor_range), &file,
-                                   &end_line, &end_column, &end_offset);
-        std::cout << " spanning lines " << start_line << " to " << end_line;
-        clang_disposeString(cursor_spelling);
-
-        std::cout << " in file " << f_name;
-        clang_disposeString(cx_file_name);
-
-        std::cout << "\n";
-        return CXChildVisit_Recurse;
-      },
-      my_file);
+      &parse_data);
 }
