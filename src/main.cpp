56bc0a9215e58cd9d6731f09048a3f2cb07f1bd3
0xd6cb6d73 <72226198+0xd6cb6d73@users.noreply.github.com>
2026-02-20 19:46:46 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,11 @@
-#include <cstdint>
-
 import ctoh;
 import std;
 
-int32_t main(int32_t argc, char *argv[]) {
+int main(int argc, char *argv[]) {
   if (argc < 2) {
     throw std::runtime_error("Missing filename");
   }
   std::string file_name = std::string(argv[1]);
 
-  ctoh::run(file_name);
+  ctoh::run(std::move(file_name));
 }
